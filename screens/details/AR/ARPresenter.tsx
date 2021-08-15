aa00d051e5686d02763bf96398b8a9068f8a27eb
okdohyuk <okdohyuk@gmail.com>
2021-08-15 22:42:14 +0900

    feat: capture functions

--- screens/details/AR/ARPresenter.tsx
+++ screens/details/AR/ARPresenter.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from 'styled-components/native';
+import {PermissionsAndroid, Platform} from 'react-native';
+import ViewShot from 'react-native-view-shot';
+import CameraRoll from '@react-native-community/cameraroll';
+
 import {Text} from '../../../components/Text';
 
 const VRWrap = styled.View`
@@ -21,10 +25,44 @@ const CaptureButtonView = styled.View`
 `;
 
 export default () => {
+  const captureRef = useRef(null);
+
+  const getPhotoUri = async () => {
+    let uri = '';
+    if (captureRef.current) {
+      uri = await captureRef.current!.capture();
+    }
+    return uri;
+  };
+
+  const hasAndroidPermission = async () => {
+    try {
+      const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+      await PermissionsAndroid.request(permission);
+      Promise.resolve();
+      console.log(Promise.resolve());
+    } catch (error) {
+      Promise.reject(error);
+      console.log(Promise.resolve(error));
+    }
+  };
+
+  const onSave = async () => {
+    if (Platform.OS === 'android') {
+      await hasAndroidPermission();
+    }
+
+    const uri = await getPhotoUri();
+    const result = await CameraRoll.save(uri);
+    console.log('🐤result', result);
+  };
+
   return (
     <VRWrap>
-      <Text>2334</Text>
-      <CaptureButton>
+      <ViewShot ref={captureRef} options={{format: 'jpg', quality: 1.0}}>
+        <Text style={{backgroundColor: '#fff'}}>hello world</Text>
+      </ViewShot>
+      <CaptureButton onPress={onSave}>
         <CaptureButtonView />
       </CaptureButton>
     </VRWrap>
--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,9 @@
 declare module 'react-viro';
+declare module 'react-native-screen-capture-secure';
+
+function createRef<T>(): RefObject<T>;
+
+interface RefObject<T> {
+  // immutable
+  readonly current: T | null;
+}
