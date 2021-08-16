b8dfb1021f615b8d19c064cad03dfad8f69af768
okdohyuk <okdohyuk@gmail.com>
2021-08-16 15:57:59 +0900

    feat: google login button

--- components/User/Login.tsx
+++ components/User/Login.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import {Text} from 'react-native';
 import styled from 'styled-components/native';
 
 const LoginWrap = styled.View`
   width: 100%;
-  height: 100px;
-  margin-bottom: 20px;
+  height: 150px;
+
   align-items: center;
   justify-content: center;
+`;
+
+const LoginButton = styled.TouchableOpacity`
+  width: 200px;
+  padding: 10px 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 5px;
+  border: solid 0.3px #000;
+`;
+
+const ButtonImage = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
+
+const ButtonText = styled.Text`
+  font-family: NanumSquareRegular;
+  font-size: 18px;
 `;
 
 function Login() {
   return (
     <LoginWrap>
-      <Text>로그인</Text>
+      <LoginButton>
+        <ButtonImage source={require('../../assets/g-logo.png')} />
+        <ButtonText>구글로 로그인</ButtonText>
+      </LoginButton>
     </LoginWrap>
   );
 }
